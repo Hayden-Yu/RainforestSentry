b62c104726eb1e7330f5f2f889482b4e74199575
hayden <hayden.yu.toronto@gmail.com>
2019-05-15 22:20:38 -0400

    request json schema validation

--- src/http/listing.ts
+++ src/http/listing.ts
@@ -1,11 +1,26 @@
 import { Router } from "express";
 import getListing from "../lib/amazon";
+import ajv from 'ajv';
+import listingSchema from '../../resources/listing.schema.json';
 
 export const listing = Router();
+const ajv_ = new ajv();
 
 listing.post('/listing', (req, res, next) => {
-  if (!req.body.url) {
-    return res.status(400).send();
+  const listingValidation = ajv_.compile(listingSchema);
+  if (!listingValidation(req.body)) {
+    const resBody = {
+      errors: <any[]>[],
+    };
+    if (listingValidation.errors && listingValidation.errors.length) {
+      resBody.errors = listingValidation.errors.map(e => {
+        return {
+          params: e.params,
+          message: e.message,
+        };
+      });
+    }
+    return res.status(400).send(resBody);
   }
   return getListing(req.body.url)
       .then(result => res.send(result));
